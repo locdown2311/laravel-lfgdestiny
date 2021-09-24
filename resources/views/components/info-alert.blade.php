0d0dd98c587e4cfb78591579b01723107375b9cd
locdown2311 <igor2311@gmail.com>
2021-09-23 21:37:11 -0300

    Adicionado card de informações e barra de busca

---
+++ resources/views/components/info-alert.blade.php
@@ -0,0 +1,17 @@
+<div>
+    <!-- Info alert -->
+    <div class="flex bg-blue-200 p-4 border border-gray-800 rounded-md shadow-sm">
+        <div class="mr-4">
+            <div class="h-10 w-10 text-white bg-blue-600 rounded-full flex justify-center items-center">
+                <i class="material-icons">Info</i>
+            </div>
+        </div>
+        <div class="flex justify-between items-center w-full">
+            <div class="text-blue-600">
+                <p class="mb-2 font-bold align-middle">
+                    {{$corpo}}
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
