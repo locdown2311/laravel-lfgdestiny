39f3b12cd2609c67ab881b96ca880b76b07ff5b1
locdown2311 <igor2311@gmail.com>
2021-10-01 00:14:45 -0300

    Softdelete em cascada

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Dyrynda\Database\Support\CascadeSoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -9,7 +10,8 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Category extends Model
 {
     use HasFactory;
-    use SoftDeletes;
+    use SoftDeletes, CascadeSoftDeletes;
+    protected $cascadeDeletes = ['activities'];
     protected $fillable=['tipo','descricao'];
 
 }
