0684f45e5afeaafa41ce28d0d20a24173a999236
locdown2311 <igor2311@gmail.com>
2021-10-01 00:24:31 -0300

    Revert "Softdelete em cascada"
    This reverts commit 39f3b12cd2609c67ab881b96ca880b76b07ff5b1.

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Dyrynda\Database\Support\CascadeSoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -10,8 +9,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Category extends Model
 {
     use HasFactory;
-    use SoftDeletes, CascadeSoftDeletes;
-    protected $cascadeDeletes = ['activities'];
+    use SoftDeletes;
     protected $fillable=['tipo','descricao'];
 
 }
